9f4d64aff2627eb13197769c16eba451d9195d9b
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-09-25 09:50:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John',
+    streetAddress: '123 Main St'
+  };
+  
+  // Function to update an employee Object with a new key-value pair (non-destructive)
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Function to update an employee Object with a new key-value pair (destructive)
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a key-value pair from an employee Object (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete a key-value pair from an employee Object (destructive)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+ 
+  
